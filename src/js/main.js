d48fce7d88b000601276e5623f7e1bbc7072bb61
Donovan Adrian <websurfer21020@gmail.com>
2021-09-16 18:43:26 -0600

    Updated main JS File
    Updated main JS file with some basic code to control the data displayed in the DOM.

--- src/js/main.js
+++ src/js/main.js
@@ -4,3 +4,120 @@
  * with written consent under any circumstance.
  */
 
+let basicModeDiv;
+let advancedModeDiv;
+let baseURLInp;
+let suffixURLInp;
+let randomSuffixTxt;
+let specificSuffixTxt;
+let showHideSearchCriteria;
+let searchCriteriaDiv;
+let searchCriteriaInp;
+let randomSearchTxt;
+let dynamicSearchTxt;
+let randCountInp;
+let activateRandomizer;
+
+
+
+window.onload = function initialize() {
+  basicModeDiv = document.getElementById('basicModeDiv');
+  advancedModeDiv = document.getElementById("advancedModeDiv");
+  baseURLInp = document.getElementById("baseURLInp");
+  suffixURLInp = document.getElementById("suffixURLInp");
+  randomSuffixTxt = document.getElementById("randomSuffixTxt");
+  specificSuffixTxt = document.getElementById("specificSuffixTxt");
+  showHideSearchCriteria = document.getElementById('showHideSearchCriteria');
+  searchCriteriaDiv = document.getElementById('searchCriteriaDiv');
+  searchCriteriaInp = document.getElementById("searchCriteriaInp");
+  randomSearchTxt = document.getElementById("randomSearchTxt");
+  dynamicSearchTxt = document.getElementById("dynamicSearchTxt");
+  randCountInp = document.getElementById("randCountInp");
+  activateRandomizer = document.getElementById("activateRandomizer");
+
+  activateRandomizer.onclick = function() {
+    fetchRandomPage();
+  }
+
+  showHideSearchCriteria.onclick = function() {
+    if (searchCriteriaDiv.style.display == "none") {
+      searchCriteriaDiv.style.display = "block";
+    } else {
+      searchCriteriaDiv.style.display = "none";
+    }
+  }
+}
+
+function fetchRandomPage() {
+  alert("Sorry bruh, this doesn't work yet!");
+  //basic or advanced check
+  //basic
+  //youtube formula: https://www.youtube.com/watch?v=88888888888 (11 numbers or letters)
+  //imgur formula: https://imgur.com/gallery/8888888 (7 numbers OR letters)
+  //wikipedia formula: https://en.wikipedia.org/wiki/88888888888888888888 (any combo of numbers or letters)
+  //advanced
+  //check all user inputs...
+  //if searchCriteriaDiv is hidden or if "how many"/"search criteria" fields are empty...
+  //https://[BaseURL]/[Suffix]/
+}
+
+/*
+*
+* DOM Functionality
+*
+ */
+
+function basicAdvancedMode(mode) {
+  console.log(mode);
+  switch (mode) {
+    case 'basic':
+      basicModeDiv.style.display = "block";
+      advancedModeDiv.style.display = "none";
+      break;
+    case 'advanced':
+      basicModeDiv.style.display = "none";
+      advancedModeDiv.style.display = "block";
+      break;
+    default:
+      console.log("Is this your card? " + mode);
+      break;
+  }
+}
+
+function swapRadio(btn) {
+  switch (btn) {
+    case 'randomSuffRad':
+      showHideTxt(randomSuffixTxt, "block");
+      showHideTxt(specificSuffixTxt, "none");
+      break;
+    case 'specificSuffRad':
+      showHideTxt(randomSuffixTxt, "none");
+      showHideTxt(specificSuffixTxt, "block");
+      break;
+    case 'dynamicTxtRad':
+      showHideTxt(randomSearchTxt, "block");
+      showHideTxt(dynamicSearchTxt, "none");
+      break;
+    case 'specificTxtRad':
+      showHideTxt(randomSearchTxt, "none");
+      showHideTxt(dynamicSearchTxt, "block");
+      break;
+    default:
+      console.log("Is this your card? " + btn);
+      break;
+  }
+}
+
+function showHideTxt(txtElement, state) {
+  try {
+    txtElement.style.display = state;
+  } catch (err) {
+    console.log("Something went wrong show/hide-ing the text!");
+  }
+}
+
+/*
+*
+* Nothing else down here!
+*
+ */
